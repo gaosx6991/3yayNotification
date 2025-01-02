5512d255abbacb4c540785e60fb0fd08b08d5aaf
gaosx <dev@gaosx.top>
2025-01-02 20:39:02 +0800

    bak

---
+++ Post图片被拒_lang.php
@@ -0,0 +1,8 @@
+array(
+	'🚫 Photos rejected!' => array(
+		'en' => '🚫 Photos rejected!',
+	),
+	'Sorry, some of your photos were rejected. Follow the rules and upload new ones! 🔄' => array(
+		'en' => 'Sorry, some of your photos were rejected. Follow the rules and upload new ones! 🔄',
+	),
+);
