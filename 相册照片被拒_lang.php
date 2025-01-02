cd0f02250752425e851fa28ca26b2b6419c873c2
gaosx <dev@gaosx.top>
2025-01-02 20:37:27 +0800

    bak

---
+++ 相册照片被拒_lang.php
@@ -0,0 +1,11 @@
+array(
+	'🚫 Photos Rejected!' => array(
+		'en' => '🚫 Photos Rejected!',
+	),
+	'Sorry, some photos in your album didn’t pass. Follow the rules and upload new ones! 📸' => array(
+		'en' => 'Sorry, some photos in your album didn’t pass. Follow the rules and upload new ones! 📸',
+	),
+	'Sorry, some photos in your partner’s album didn’t pass. Follow the rules and upload new ones! 📸' => array(
+		'en' => 'Sorry, some photos in your partner’s album didn’t pass. Follow the rules and upload new ones! 📸',
+	),
+);
