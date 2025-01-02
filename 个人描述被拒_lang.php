54c4e873c2b93c44afcb2b42ff725c4c8da68809
gaosx <dev@gaosx.top>
2025-01-02 20:41:06 +0800

    bak

---
+++ 个人描述被拒_lang.php
@@ -0,0 +1,11 @@
+array(
+	'🚫 Bio Rejected!' => array(
+		'en' => '🚫 Bio Rejected!',
+	),
+	'Sorry, your bio didn’t pass. Review the guidelines and submit a revised version! ✍️' => array(
+		'en' => 'Sorry, your bio didn’t pass. Review the guidelines and submit a revised version! ✍️',
+	),
+	'Sorry, your partner’s bio didn’t pass. Review the guidelines and submit a revised version! ✍️' => array(
+		'en' => 'Sorry, your partner’s bio didn’t pass. Review the guidelines and submit a revised version! ✍️',
+	),
+);
